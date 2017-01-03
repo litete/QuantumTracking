77c676fc67c94f662ed32df3c4f272b17a8d1c7a
litete <a8092667>
2017-01-03 09:55:41 +0800

    数据库为ctbri了

---
+++ src/app/home/model/ResultTJdBar.java
@@ -0,0 +1,34 @@
+package app.home.model;
+
+/**
+ * Created by Administrator on 2017/1/3.
+ */
+public class ResultTJdBar {
+    private String product_brand;
+    private double PC;
+    private double Mobile;
+
+    public String getProduct_brand() {
+        return product_brand;
+    }
+
+    public void setProduct_brand(String product_brand) {
+        this.product_brand = product_brand;
+    }
+
+    public double getPC() {
+        return PC;
+    }
+
+    public void setPC(double PC) {
+        this.PC = PC;
+    }
+
+    public double getMobile() {
+        return Mobile;
+    }
+
+    public void setMobile(double mobile) {
+        Mobile = mobile;
+    }
+}
